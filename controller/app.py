2703b20eca41c92f5815460a8360dbc5ac413e8f
Tevin Zhang <mail2tevin@gmail.com>
2015-03-28 15:42:55 +0800

    [controller]eliminate data process

--- controller/app.py
+++ controller/app.py
@@ -15,10 +15,6 @@ TRIG = 20
 ECHO = 21
 TIME_BREAK = 0.5
 
-MAX = 50
-MIN = 2
-LEVELS = 7
-STEP = MAX / LEVELS
 WS_URL = "ws://121.41.107.136:9300/"
 
 
@@ -26,14 +22,9 @@ def main():
     server = create_connection(WS_URL)
     sensor = ultrasound.Ultrasound(TRIG, ECHO, TIME_BREAK)
     while True:
-        distance = sensor.get_distance()
-        if distance > MAX or distance < MIN:
-            distance = 0
-        level = int((distance - MIN) / (MAX - MIN) * 100 / STEP)
-        if level < 0:
-            level = 0
-        server.send(str(level))
-        print distance, level
+        distance = int(sensor.get_distance())
+        server.send(str(distance))
+        print distance
         time.sleep(0.2)
 
 if __name__ == "__main__":
